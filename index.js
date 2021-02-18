96e52990ddebbf4a889a06e1dcbe7ec93f6538a9
Andrei <andreiilinescu19@gmail.com>
2021-02-18 13:00:51 +0200

    test

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use(vhost('gryd.tech', function (req, res) {
-    // handle req + res belonging to mail.example.com
-    res.text('yes');
-  }))
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
